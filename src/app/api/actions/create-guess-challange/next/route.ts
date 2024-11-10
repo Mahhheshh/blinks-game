1a8f8daeca725e0d371e602149118ab93250a0d7
Mahhheshh <100200105+Mahhheshh@users.noreply.github.com>
2024-11-11 00:16:23 +0530

    update the blinks completion route

--- src/app/api/actions/create-guess-challange/next/route.ts
+++ src/app/api/actions/create-guess-challange/next/route.ts
@@ -12,12 +12,17 @@ export const GET = async (req: Request) => {
 export const OPTIONS = async () => Response.json(null, { headers });
 
 export const POST = async (req: Request) => {
+  const baseUrl = new URL(req.url);
+  console.log(baseUrl);
+  const challengeId = baseUrl.searchParams.get("challenge-id");
+  const href = new URL(`/api/actions/guess?challenge-id=${challengeId}`, baseUrl.origin).toString();
+  const description = `Visit https://dial.to/?action=solana-action:${href} to participate in the challenge!`;
   const payload: CompletedAction = {
     type: "completed",
-    title: "Your Guess Challange has been created successfully!",
-    icon: new URL("/solana_devs.jpg", new URL(req.url).origin).toString(),
-    label: "Catoff Guess Challange Created",
-    description: "some message",
+    title: "Guess Challenge Created Successfully!",
+    icon: new URL("/solana_devs.jpg", baseUrl.origin).toString(),
+    label: "Challenge Created",
+    description: description,
   };
 
   return Response.json(payload, { headers });
